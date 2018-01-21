c884d75e8d070c6b69f76a60302e408a3ab3e33f
Kenny <ken12938@yahoo.com>
2018-01-20 21:45:59 -0500

    Alchemy example

---
+++ createAlchemyJSON.py
@@ -0,0 +1,35 @@
+import json
+
+graph = {}
+graph['nodes'] = []
+graph['edges'] = []
+
+f = open('names.txt', 'r')
+namesText = f.read()
+lines = namesText.split('\n')
+for i in range(0, len(lines)):
+    currStr = lines[i]
+    id = currStr[0:24]
+    name = currStr[25:]
+
+    currNode = {}
+    currNode['id'] = id
+    currNode['caption'] = name
+    graph['nodes'].append(currNode)
+
+f = open('related.txt', 'r')
+relatedText = f.read()
+lines = relatedText.split('\n')
+for i in range(0, len(lines)):
+    currStr = lines[i].replace('[', '').replace(']', '').replace(',', '').strip()
+    neighbors = currStr.split(' ')
+    currNode = neighbors[0]
+    del neighbors[0]
+    for j in range(0, len(neighbors)):
+        edge = {}
+        edge['source'] = currNode
+        edge['target'] = neighbors[j]
+        graph['edges'].append(edge)
+
+with open('graph.json', 'w') as outfile:
+    json.dump(graph, outfile)

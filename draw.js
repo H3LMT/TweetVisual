91e87660409d9d48619f8c5477417811c8de6455
H3LMT <vishalbaskar@msn.com>
2018-01-21 04:39:06 -0500

    adding link stuff

---
+++ draw.js
@@ -0,0 +1,199 @@
+var related = "UCZSVvDqRX9_b377Jta8eA_Q []\nUC11PvrGPzo6Y7Zc6-e9cAKg [UCRtTcZgg9MekxGVTMXy7Epg, UCHCZlTMfz0pEKSVX_gqvX5A, UCDo9msNItILnyF_Y2eHaNQg, UCVtFOytbRpEvzLjvqGG5gxQ, UCZSVvDqRX9_b377Jta8eA_Q, UCcgVECVN4OKV6DH1jLkqmcA, UCG8rbF3g2AMX70yOd8vqIZg, UC-lHJZR3Gqxm24_Vd_AJ5Yw]\nUCG8rbF3g2AMX70yOd8vqIZg [UCbwMZHCMpBiOWfYLBOH-9Qw, UCcgVECVN4OKV6DH1jLkqmcA, UC11PvrGPzo6Y7Zc6-e9cAKg, UCZSVvDqRX9_b377Jta8eA_Q, UCWKiQZOS8dhDewYoABsapgA, UC-SV8-bUJfXjrRMnp7F8Wzw]\nUCcgVECVN4OKV6DH1jLkqmcA [UCbEn1kwHr0TPmC9VPWIA_Mw, UC3FetBdeuG34fMqXIm0sjSA, UCUzoUpeyYGV1ijrgXJwdjuQ, UCxolq2pCRDttlLQcd8r1w7A, UCrCkLipGria4UloMpvNil5g, UCdqcWDUu8QZuk4srlyMyy5Q, UCPSKH78KkgzNlQAEIXBLS0Q, UCLO5YnTRG8jySOUeU2ohnpw, UCe2ba_7LwQdNK5BKFSLm41Q, UCyLT9kuLqDYO-mr46ay3qmw, UCG8rbF3g2AMX70yOd8vqIZg, UC11PvrGPzo6Y7Zc6-e9cAKg, UCdD2tf-YhMHApzfHbh4qI8g]\nUC-lHJZR3Gqxm24_Vd_AJ5Yw [UCG8rbF3g2AMX70yOd8vqIZg, UCNvzD7Z-g64bPXxGzaQaa4g, UCo_IB5145EVNcf8hw1Kku7w, UCKqH_9mk1waLgBiL2vT5b9g]\nUCNvzD7Z-g64bPXxGzaQaa4g [UCpFHkjOa7ia6bH5_6cDsDXg, UC7s6t5KCNwRkb7U_3-E1Tpw, UCWkdg1JlFH--qu0HVN_RBow, UCI4D2tSAiHqZBRB67nTKqww, UCLcQXq5mn1Jd2VS4heis7tw, UC-lHJZR3Gqxm24_Vd_AJ5Yw, UCKy1dAqELo0zrOtPkf0eTMw, UCvO9Xk3bheuxEemvknCj72g, UC4w_tMnHl6sw5VD93tVymGw, UCodK-9eXEm_hWSDDfLr4QzA, UCo_IB5145EVNcf8hw1Kku7w]\nUCKqH_9mk1waLgBiL2vT5b9g [UCcWl6q7rcJ9WoF_Kwmxg23A, UCQiojTHkAvFvdCSajklqUXA, UCClNRixXlagwAd--5MwJKCw, UCsbTG6lBt0ZMJCsFoR-Rh9w, UCNPCx6BWK0ID6UKO1QicOmQ, UC1o9zvRwIqP_K5ByWs-EVwA, UCvPW1W4WlpTgNezZzwIstLA, UCj8IEH-pSJU8_xEN31K5nKw, UC-kOXc3gBwksVfmndSEz7jg, UCbgtuRStKI_5ACSjubSTUPA, UCYzPXprvl5Y-Sf0g4vX-m6g, UC-lHJZR3Gqxm24_Vd_AJ5Yw]\nUCbwMZHCMpBiOWfYLBOH-9Qw [UCG8rbF3g2AMX70yOd8vqIZg, UCcgVECVN4OKV6DH1jLkqmcA, UCWKiQZOS8dhDewYoABsapgA, UC04HB4E7fowxQwTks88p_iA, UCi9cDo6239RAzPpBZO9y5SA, UCY30JRSgfhYXA6i6xX1erWg]\nUCWKiQZOS8dhDewYoABsapgA [UCG8rbF3g2AMX70yOd8vqIZg, UCcgVECVN4OKV6DH1jLkqmcA, UCdvFVP7-CZTlAuL7FqJcUZg, UCLjBCYil92x3SYtHrvr3Pew, UCHtXqf99mHAh_sXEhGWDCyQ, UCuG0IsfYerpuXLPQwFvRbXQ]\nUCo_IB5145EVNcf8hw1Kku7w [UCAaegDUlb7doIKo0Rc1F08g, UC3sznuotAs2ohg_U__Jzj_Q, UCuuwzVijgW6Z3sQ49pkisfw, UCyS4xQE6DK4_p3qXQwJQAyA, UCs02eesk_kskNFkQv07Y3cw, UCxNcCtPqqGIMEDwDACPVoHg, UCMkhi-z4ytE2HSNBzQJnERg, UC7gnV9U47_yawcpfOUnipSw, UC6-ymYjG0SU0jUWnWh9ZzEQ, UCPYJR2EIu0_MJaDeSGwkIVw, UC7_YxT-KID8kRbqZo7MyscQ, UCYzPXprvl5Y-Sf0g4vX-m6g, UC-lHJZR3Gqxm24_Vd_AJ5Yw, UC9CuvdOVfMPvKCiwdGKL3cQ]";
+var names = "UCHtXqf99mHAh_sXEhGWDCyQ Mark Dohner\nUCvO9Xk3bheuxEemvknCj72g WhatCulture Gaming\nUCLjBCYil92x3SYtHrvr3Pew George Janko\nUCcgVECVN4OKV6DH1jLkqmcA Jake Paul\nUCsbTG6lBt0ZMJCsFoR-Rh9w Rynx\nUCbgtuRStKI_5ACSjubSTUPA BasicallyIDoWrk\nUC-lHJZR3Gqxm24_Vd_AJ5Yw PewDiePie\nUC6-ymYjG0SU0jUWnWh9ZzEQ Wisecrack\nUCNPCx6BWK0ID6UKO1QicOmQ Mr Sark\nUCe2ba_7LwQdNK5BKFSLm41Q Tessa Brooks\nUCY30JRSgfhYXA6i6xX1erWg Smosh\nUC7s6t5KCNwRkb7U_3-E1Tpw BECAUSEVIDEOGAMES\nUCUzoUpeyYGV1ijrgXJwdjuQ Jake Paul Biz\nUCAaegDUlb7doIKo0Rc1F08g GTLive\nUC3sznuotAs2ohg_U__Jzj_Q The Film Theorists\nUC4w_tMnHl6sw5VD93tVymGw The Know\nUC7gnV9U47_yawcpfOUnipSw TheNationalDex\nUCKy1dAqELo0zrOtPkf0eTMw IGN\nUCuuwzVijgW6Z3sQ49pkisfw Carf Darko\nUCDo9msNItILnyF_Y2eHaNQg RiceGum\nUCWkdg1JlFH--qu0HVN_RBow CabooseXBL\nUCxolq2pCRDttlLQcd8r1w7A Chance and Anthony\nUCG8rbF3g2AMX70yOd8vqIZg Logan Paul Vlogs\nUCvPW1W4WlpTgNezZzwIstLA Daithi De Nogla\nUCbEn1kwHr0TPmC9VPWIA_Mw Team 10\nUCPSKH78KkgzNlQAEIXBLS0Q Nick Crompton\nUCuG0IsfYerpuXLPQwFvRbXQ Ayla Woodruff\nUCodK-9eXEm_hWSDDfLr4QzA Pretty Good Gaming\nUCClNRixXlagwAd--5MwJKCw H2ODelirious\nUCdqcWDUu8QZuk4srlyMyy5Q Kade Speiser\nUCNvzD7Z-g64bPXxGzaQaa4g gameranx\nUCcWl6q7rcJ9WoF_Kwmxg23A Lui Calibre\nUCdvFVP7-CZTlAuL7FqJcUZg Brendan North\nUCHCZlTMfz0pEKSVX_gqvX5A Baited!\nUCj8IEH-pSJU8_xEN31K5nKw fourzer0seven\nUC9CuvdOVfMPvKCiwdGKL3cQ GameGrumps\nUCMkhi-z4ytE2HSNBzQJnERg DigressingNSQ\nUCRtTcZgg9MekxGVTMXy7Epg KEEMSTAR\nUC1o9zvRwIqP_K5ByWs-EVwA BigJigglyPanda\nUCdD2tf-YhMHApzfHbh4qI8g Justin Roberts\nUC04HB4E7fowxQwTks88p_iA Erika paul\nUCKqH_9mk1waLgBiL2vT5b9g VanossGaming\nUCLO5YnTRG8jySOUeU2ohnpw Tristan Tales\nUCpFHkjOa7ia6bH5_6cDsDXg Complex News\nUCyS4xQE6DK4_p3qXQwJQAyA DidYouKnowGaming?\nUCs02eesk_kskNFkQv07Y3cw Trailer Drake\nUC-kOXc3gBwksVfmndSEz7jg I AM WILDCAT\nUC7_YxT-KID8kRbqZo7MyscQ Markiplier\nUCyLT9kuLqDYO-mr46ay3qmw Chad Tepper\nUCPYJR2EIu0_MJaDeSGwkIVw The Completionist\nUC-SV8-bUJfXjrRMnp7F8Wzw RomanAtwoodVlogs\nUCQiojTHkAvFvdCSajklqUXA Terroriser\nUCI4D2tSAiHqZBRB67nTKqww TPS\nUCbwMZHCMpBiOWfYLBOH-9Qw TheOfficialLoganPaul\nUCo_IB5145EVNcf8hw1Kku7w The Game Theorists\nUCYzPXprvl5Y-Sf0g4vX-m6g jacksepticeye\nUCi9cDo6239RAzPpBZO9y5SA Lele Pons\nUCZSVvDqRX9_b377Jta8eA_Q Kavos\nUC3FetBdeuG34fMqXIm0sjSA Jake Paul 2\nUCrCkLipGria4UloMpvNil5g Erika Costell\nUCWKiQZOS8dhDewYoABsapgA Andy Altig\nUCVtFOytbRpEvzLjvqGG5gxQ KSI\nUC11PvrGPzo6Y7Zc6-e9cAKg DramaAlert\nUCLcQXq5mn1Jd2VS4heis7tw WASTETIME\nUCxNcCtPqqGIMEDwDACPVoHg Gaijin Goombah Media\n";
+
+
+
+function Node(id, children) { //children is an array of strings representing id's of other nodes
+    this.id = id;
+    this.children = children;
+    this.x = Math.random() * (width - 100) + 200;
+    this.y = Math.random() * (height - 100) + 200;
+}
+
+function addName(node, name) { //node is an obj created from Node() method
+    node.name = name;
+    return node;
+}
+
+var width = 1500;
+var height = 900;
+var nodeList = [];
+var data = ["Channel B", "Channel E", "Channel G", "Channel M"];
+
+var relatedTextByLine = related.split("\n");
+
+for(var line = 0; line < relatedTextByLine.length; line++){
+    var string = relatedTextByLine[line].replace(/\[/g, "").replace(/\]/g, "").replace(/,/g, "");
+    var neighbors = string.split(" "); //Will remove the first element, which is the current node
+    var currNode = neighbors[0];
+    neighbors.splice(0, 1);
+
+    nodeList.push(new Node(currNode, neighbors));
+}
+
+var namesTextByLine = names.split("\n");
+
+for(var line = 0; line < namesTextByLine.length; line++) {
+    var string = namesTextByLine[line];
+    var id = string.substring(0, 24);
+    var name = string.substring(25, string.length);
+
+    for(var node = 0; node < nodeList.length; node++) {
+        if(id.trim() == (nodeList[node]).id.trim()) {
+            nodeList[node] = addName(nodeList[node], name);
+        }
+    }
+}
+
+//Begin using d3
+
+var x = [];
+var y = [];
+
+for(var i = 0; i < nodeList.length; i++) {
+    x.push(Math.random() * (width - 100) + 200);
+    y.push(Math.random() * (height - 100) + 200);
+}
+
+var circleRadius = Math.random() * width / 100 + width / 40;
+var elnum = 0; //Element number
+
+var scale = d3.scaleLinear().domain([0, d3.max(nodeList)]).range([0, width]);
+
+var graph = d3.select(".graph")
+    .attr("width", width)
+    .attr("height", height);
+
+var nodes = graph.selectAll("g").data(nodeList)
+    .enter().append("g")
+    .attr("transform", function(d, i) {
+        var s = "translate(" + d.x + ", " + d.y + ")";
+        return s;
+    });
+
+//Draw circles first
+
+nodes.append("circle")
+    .attr("r", circleRadius)
+    .attr("fill", "white")
+    .attr("stroke", "purple")
+    .attr("stroke-width", "3");
+
+nodes.append("text")
+    .attr("x", -30)
+    .attr("y", -3)
+    .attr("dy", ".35em")
+    .attr("fill", "blue")
+    .attr("stroke", "purple")
+    .text(function(d) { 
+        return d.name; 
+    });
+
+for (var node = 0; node < nodeList.length; node++) {
+    console.log("Reached here");
+    var currNode = nodeList[node];
+    for(var c = 0; c < currNode.children.length; c++) {
+        var child = currNode.children[c];
+        console.log(child);
+        var nx;
+        var ny;
+
+        for (var h = 0; h < nodeList.length; h++) {
+            if(nodeList[h].id.trim() == child.trim()) {
+                nx = nodeList[h].x;
+                ny = nodeList[h].y;
+            }
+        }
+
+        var xDist = currNode.x - nx;
+        var yDist = currNode.y - ny;
+        var dist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+        var x1 = currNode.x - circleRadius*xDist/dist;
+        var y1 = currNode.y - circleRadius*yDist/dist;
+        var x2 = nx + (circleRadius+10)*xDist/dist;
+        var y2 = ny + (circleRadius+10)*yDist/dist;
+
+        graph.append("line")
+            .attr("x1", x1)
+            .attr("y1", y1)
+            .attr("x2", x2)
+            .attr("y2", y2)
+            .attr("stroke", "purple")
+            .attr("stroke-width", 3)
+            .attr("marker-end", "url(#arrow)");
+    }
+}
+
+
+
+/*//Begin using d3
+
+var x = [];
+var y = [];
+
+for(var i = 0; i < data.length; i++) {
+    x.push(Math.random() * (width - 100) + 100);
+    y.push(Math.random() * (height - 100) + 100);
+}
+
+var circleRadius = Math.random() * width / 50 + width / 20;
+var elnum = 0; //Element number
+
+var scale = d3.scaleLinear().domain([0, d3.max(data)]).range([0, width]);
+
+var graph = d3.select(".graph")
+    .attr("width", width)
+    .attr("height", height);
+
+var nodes = graph.selectAll("g").data(data)
+    .enter().append("g")
+    .attr("transform", function(d, i) {
+        var s = "translate(" + x[elnum] + ", " + y[elnum] + ")";
+        elnum++;
+        return s;
+    });
+
+//First draw circles
+
+nodes.append("circle")
+    .attr("r", circleRadius)
+    .attr("fill", "white")
+    .attr("stroke", "purple")
+    .attr("stroke-width", "3");
+
+nodes.append("text")
+    .attr("x", -30)
+    .attr("y", -3)
+    .attr("dy", ".35em")
+    .attr("fill", "blue")
+    .attr("stroke", "purple")
+    .text(function(d) { 
+        return d; });
+
+//Will now draw lines
+
+for(var i = 0; i < data.length; i++) {
+    for(var j = i + 1; j < data.length; j++) {
+        //Calculating coordinates of line
+        var xDist = x[i] - x[j];
+        var yDist = y[i] - y[j];
+        var dist = Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+        console.log(xDist);
+        console.log(yDist);
+        console.log(dist);
+        console.log(circleRadius);
+        var x1 = x[i] - circleRadius*xDist/dist;
+        var y1 = y[i] - circleRadius*yDist/dist;
+        var x2 = x[j] + (circleRadius+10)*xDist/dist;
+        var y2 = y[j] + (circleRadius+10)*yDist/dist;
+
+        graph.append("line")
+            .attr("x1", x1)
+            .attr("y1", y1)
+            .attr("x2", x2)
+            .attr("y2", y2)
+            .attr("stroke", "purple")
+            .attr("stroke-width", 3)
+            .attr("marker-end", "url(#arrow)");
+    }
+}*/
